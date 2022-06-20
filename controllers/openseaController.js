9a1516abd976f62d823c856b2d17c99609940607
ehahaC <ehahac@gmail.com>
2022-06-20 18:08:36 +0800

    feat: demo

--- controllers/openseaController.js
+++ controllers/openseaController.js
@@ -5,7 +5,7 @@ puppeteer
         headless: false,
         devtools: true,
         // executablePath: '/usr/bin/chromium',
-        // args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
         // args: ['--no-sandbox']
     })
     .then((browser) => {
@@ -24,7 +24,7 @@ puppeteer
  */
 exports.getData = async function (req, res, next) {
     const { contractAddress } = req.params;
-
+    console.log("contractAddress", contractAddress);
     try {
         let data = await getData(contractAddress);
 
@@ -51,21 +51,34 @@ function getData(contractAddress) {
         // const page = await browser.newPage();
 
         try {
-            let result = await global.page.evaluate(async (contractAddress) => {
-                try {
-                    let response = await fetch(
-                        `https://api.opensea.io/api/v1/asset_contract/${contractAddress}`
-                    );
+            // let result = await global.page.evaluate(async (contractAddress) => {
+            //     try {
+            //         let response = await fetch(
+            //             `https://api.opensea.io/api/v1/asset_contract/${contractAddress}`
+            //         );
 
-                    if (response.status !== 200) {
-                        return Promise.reject("请求失败");
-                    }
-                    return Promise.resolve(await response.json());
-                } catch (error) {
-                    return Promise.reject(error);
-                }
-            }, contractAddress);
-            resovle(result);
+            //         if (response.status !== 200) {
+            //             return Promise.reject("请求失败");
+            //         }
+            //         return Promise.resolve(await response.json());
+            //     } catch (error) {
+            //         return Promise.reject(error);
+            //     }
+            // }, contractAddress);
+            // resovle(result);
+
+            await global.page.goto("https://api.opensea.io/api/v1/asset_contract/0x06012c8cf97bead5deae237070f9587f8e7a266d")
+            await global.page.waitForSelector('.response-info > .prettyprint')
+
+            const data = await global.page.evaluate(() => {
+                // 删除不必要节点
+                document.querySelector(".response-info > .prettyprint > span.meta.nocode").remove()
+                let preNode = document.querySelector(".response-info > .prettyprint");
+                const JSON_TEXT = preNode.textContent
+                return JSON.parse(JSON_TEXT)
+            })
+            console.log("data", data);
+            resovle(data)
         } catch (error) {
             reject(error);
         }
